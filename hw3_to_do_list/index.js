ea26af8e388e2e74ee03f64e9d918c801405ca8f
Eva <evianna.luchina.77@gmail.com>
2026-04-20 15:57:34 +0300

    hw3

---
+++ hw3_to_do_list/index.js
@@ -0,0 +1,60 @@
+const taskInput = document.getElementById('taskInput');
+const addBtn = document.getElementById('addBtn');
+
+const allBtn = document.getElementById('allBtn');
+const inProgressBtn = document.getElementById('inProgressBtn');
+const finishedBtn = document.getElementById('finishedBtn');
+
+const taskList = document.getElementById('taskList')
+
+addBtn.addEventListener('click', () => {
+    const taskText = taskInput.value.trim();
+    if(!taskText) {
+        return;
+    }
+    
+    const li = document.createElement('li');
+
+    li.innerHTML = `
+    <input type="checkbox" class="checkbox">
+    <span>${taskText}</span>
+    <button class="delete-btn"><i class="fa-solid fa-delete-left"></i></button>`;
+
+    taskList.append(li);
+    taskInput.value = '';
+
+    const checkbox = li.querySelector('.checkbox');
+    const deleteBtn = li.querySelector('.delete-btn');
+
+    checkbox.addEventListener('click', () => {
+        li.classList.toggle('completed');
+    });
+
+    deleteBtn.addEventListener('click', () => {
+        li.remove();
+    });
+
+    allBtn.addEventListener('click', () => {
+        li.style.display = 'flex';
+    });
+
+    inProgressBtn.addEventListener('click', () => {
+        if (checkbox.checked){
+            li.style.display = 'none';
+        }
+        else {
+            li.style.display = 'flex';
+        }
+    });
+
+    finishedBtn.addEventListener('click', () => {
+        if (checkbox.checked){
+            li.style.display = 'flex';
+        }
+        else {
+            li.style.display = 'none';
+        }
+    });
+    
+});
+
